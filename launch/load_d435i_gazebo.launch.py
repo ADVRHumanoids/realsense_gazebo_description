cbb8348231ba0fd212954e57c2af5802dc37c740
Alessio Lovato <alessio.lovato@iit.it>
2026-04-14 23:22:21 +0200

    (feat) Added gazebo xacro to insert camera and depth camera. The option 'align_depth' allows to create the depth camera with same spec as the color one in the color_frame so that it mimics the behavior of the realsense.
    (feat) Created basic world file in'worlds' to allow the usage of the sensor plugin in gazebo (necessary to load the sensors in the simulation.
    
    (refactor) Updated launch file to not use the Tf publisher since the pose is alredy passed to the URDF via parameter and added Gazebo Harmonic bridges to republish the topics in ROS2.

--- launch/load_d435i_gazebo.launch.py
+++ launch/load_d435i_gazebo.launch.py
@@ -18,13 +18,16 @@ from launch import LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
     OpaqueFunction,
+    IncludeLaunchDescription
 )
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 from launch_ros.actions import Node
-import launch.events
+from launch_ros.substitutions import FindPackageShare
+
 from ament_index_python.packages import get_package_share_directory
-import sys
 import xacro
 
 def launch_setup(context, *args, **kwargs):
@@ -38,6 +41,7 @@ def launch_setup(context, *args, **kwargs):
         mappings={
             'use_nominal_extrinsics': 'true', 
             'add_plug': 'false',
+            'use_mesh': 'true',
             'name': LaunchConfiguration("name").perform(context),
             'parent': LaunchConfiguration("parent").perform(context), 
             'x': LaunchConfiguration("pose_x").perform(context), 
@@ -47,7 +51,9 @@ def launch_setup(context, *args, **kwargs):
             'yaw': LaunchConfiguration("pose_yaw").perform(context), 
             'pitch': LaunchConfiguration("pose_pitch").perform(context), 
             'publish_pointcloud': LaunchConfiguration("publish_pointcloud").perform(context), 
-            'align_depth': LaunchConfiguration("align_depth").perform(context), 
+            'align_depth': LaunchConfiguration("align_depth").perform(context),
+            'gazebo_urdf': LaunchConfiguration("gazebo_urdf").perform(context),
+            'infra_enable': LaunchConfiguration("infra_enable").perform(context),
         },
     )
     urdf = {"robot_description": robot_description_content}
@@ -74,41 +80,15 @@ def launch_setup(context, *args, **kwargs):
             "use_sim_time": (LaunchConfiguration("use_sim_time").perform(context) == True),
         }],
     ))
-    nodes_to_start.append(Node (
-        name='realsense_world_tf_pub',
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        namespace='',
-        output='screen',
-        condition=IfCondition(LaunchConfiguration("pub_world_tf")),
-        arguments=[
-            "--frame-id", "world",
-            "--child-frame-id", "D435i_camera_bottom_screw_frame",
-            "--x", LaunchConfiguration("pose_x").perform(context),
-            "--y", LaunchConfiguration("pose_y").perform(context),
-            "--z", LaunchConfiguration("pose_z").perform(context),
-            "--roll", LaunchConfiguration("pose_roll").perform(context),
-            "--pitch", LaunchConfiguration("pose_pitch").perform(context),
-            "--yaw", LaunchConfiguration("pose_yaw").perform(context),
 
-        ],
-    ))
-
-    ###
-    ### SIMULATOR TODO
-    ###
-    # world_file = os.path.join(
-    #     get_package_share_path("doosan_gazebo"), "world", "doosan.world"
-    # )
-    # ignition_simulator_node = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource([
-    #         FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"
-    #     ], ),
-    #     launch_arguments={
-    #         "gz_args": f" -r -v 1 {world_file}",
-    #     }.items(),
-    #     condition=IfCondition(simulator)
-    # )
+    ignition_simulator_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"
+        ], ),
+        launch_arguments={
+            "gz_args": f" -r -v 1 {LaunchConfiguration('world_file').perform(context)}",
+        }.items(),
+    )
 
     nodes_to_start.append(Node (
         name='urdf_spawner',
@@ -120,16 +100,70 @@ def launch_setup(context, *args, **kwargs):
             "-name", LaunchConfiguration("name").perform(context),
             #not sure why this must be doubled but it is like this the topic
             "-topic", "robot_description",
-            "-x", LaunchConfiguration('spawn_x').perform(context),
-            "-y", LaunchConfiguration('spawn_y').perform(context),
-            "-z", LaunchConfiguration('spawn_z').perform(context),
-            "-R", LaunchConfiguration('spawn_roll').perform(context),
-            "-P", LaunchConfiguration('spawn_pitch').perform(context),
-            "-Y", LaunchConfiguration('spawn_yaw').perform(context),
+            # "-x", LaunchConfiguration('spawn_x').perform(context),
+            # "-y", LaunchConfiguration('spawn_y').perform(context),
+            # "-z", LaunchConfiguration('spawn_z').perform(context),
+            # "-R", LaunchConfiguration('spawn_roll').perform(context),
+            # "-P", LaunchConfiguration('spawn_pitch').perform(context),
+            # "-Y", LaunchConfiguration('spawn_yaw').perform(context),
         ],
     ))
 
-    return nodes_to_start
+    # Gz sim bridge to ROS2
+    if (LaunchConfiguration("align_depth").perform(context).lower() == "true"):
+        bridge_topics = [
+            # RGB Image topic is bridged with ros_gz_image
+            f'/{LaunchConfiguration("name").perform(context)}/color/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+            f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
+            f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+            f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/image_raw/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
+        ]
+        remappings = [
+            (f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/image_raw/points', 
+                f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/points')]
+    else:
+        bridge_topics = [
+            # RGB Image topic is bridged with ros_gz_image
+            f'/{LaunchConfiguration("name").perform(context)}/color/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+            f'/{LaunchConfiguration("name").perform(context)}/depth/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
+            f'/{LaunchConfiguration("name").perform(context)}/depth/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+            f'/{LaunchConfiguration("name").perform(context)}/depth/image_raw/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
+        ]
+        remappings = [
+            (f'/{LaunchConfiguration("name").perform(context)}/depth/image_raw/points', 
+                f'/{LaunchConfiguration("name").perform(context)}/depth/points')]
+        
+    # Infrared bridging
+    if (LaunchConfiguration("infra_enable").perform(context).lower() == "true"):
+        bridge_topics.extend([
+            f'/{LaunchConfiguration("name").perform(context)}/infra1/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
+            f'/{LaunchConfiguration("name").perform(context)}/infra1/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+            f'/{LaunchConfiguration("name").perform(context)}/infra2/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
+            f'/{LaunchConfiguration("name").perform(context)}/infra2/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+        ])
+
+    # RGB image uses ros_gz_image bridge and can be remapped per camera name.
+    nodes_to_start.append(
+        Node(
+            package='ros_gz_image',
+            executable='image_bridge',
+            name=f'{LaunchConfiguration("name").perform(context)}_color_bridge',
+            arguments=[f'/{LaunchConfiguration("name").perform(context)}/color/image_raw'],
+        )
+    )
+
+    nodes_to_start.append(
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='ros_gz_bridge',
+            arguments=bridge_topics,
+            remappings=remappings,
+        )
+    )
+
+
+    return [ignition_simulator_node] + nodes_to_start
 
 def generate_launch_description():
 
@@ -140,7 +174,6 @@ def generate_launch_description():
     declared_arguments.append(DeclareLaunchArgument("use_sim_time", default_value="true"))
     declared_arguments.append(DeclareLaunchArgument("rviz", default_value="false"))
     declared_arguments.append(DeclareLaunchArgument("robot_state_publisher", default_value="true"))
-    declared_arguments.append(DeclareLaunchArgument("pub_world_tf", default_value="false"))
     declared_arguments.append(DeclareLaunchArgument("use_sim_time", default_value="true"))
 
     declared_arguments.append(DeclareLaunchArgument("pose_x", default_value="0.0"))
@@ -150,16 +183,18 @@ def generate_launch_description():
     declared_arguments.append(DeclareLaunchArgument("pose_pitch", default_value="0.0"))
     declared_arguments.append(DeclareLaunchArgument("pose_yaw", default_value="0.0"))
 
-    declared_arguments.append(DeclareLaunchArgument("gazebo", default_value="false"))
-    declared_arguments.append(DeclareLaunchArgument("spawn_x", default_value="0.0"))
-    declared_arguments.append(DeclareLaunchArgument("spawn_y", default_value="0.0"))
-    declared_arguments.append(DeclareLaunchArgument("spawn_z", default_value="0.0"))
-    declared_arguments.append(DeclareLaunchArgument("spawn_roll", default_value="0.0"))
-    declared_arguments.append(DeclareLaunchArgument("spawn_pitch", default_value="0.0"))
-    declared_arguments.append(DeclareLaunchArgument("spawn_yaw", default_value="0.0"))
+    declared_arguments.append(DeclareLaunchArgument("world_file", default_value =
+        get_package_share_directory("realsense_gazebo_description") + "/worlds/empty.sdf"))
+    # declared_arguments.append(DeclareLaunchArgument("spawn_x", default_value="0.0"))
+    # declared_arguments.append(DeclareLaunchArgument("spawn_y", default_value="0.0"))
+    # declared_arguments.append(DeclareLaunchArgument("spawn_z", default_value="0.2"))
+    # declared_arguments.append(DeclareLaunchArgument("spawn_roll", default_value="0.0"))
+    # declared_arguments.append(DeclareLaunchArgument("spawn_pitch", default_value="0.0"))
+    # declared_arguments.append(DeclareLaunchArgument("spawn_yaw", default_value="0.0"))
     declared_arguments.append(DeclareLaunchArgument("publish_pointcloud", default_value="false"))
-    declared_arguments.append(DeclareLaunchArgument("align_depth", default_value="true"))
-
+    declared_arguments.append(DeclareLaunchArgument("align_depth", default_value="false"))
+    declared_arguments.append(DeclareLaunchArgument("gazebo_urdf", default_value="true"))
+    declared_arguments.append(DeclareLaunchArgument("infra_enable", default_value="false"))
 
 
     return LaunchDescription(
