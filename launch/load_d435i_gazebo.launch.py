684ca108e3d0b0b8b4710302f6755141dc1024ee
Alessio Lovato <alessio.lovato@iit.it>
2026-04-15 13:52:49 +0000

    (feat) Added IMU sensor to the D435i model.
    (feat) Added pointcloud topic bridge to ROS 2 depending on the 'publish_pointcloud' parameter. Gazebo Harmonic always publishes the topic. Removeed parameter from xacro.
    
    (feat) Added noise to sensors in sdf.

--- launch/load_d435i_gazebo.launch.py
+++ launch/load_d435i_gazebo.launch.py
@@ -49,8 +49,7 @@ def launch_setup(context, *args, **kwargs):
             'z': LaunchConfiguration("pose_z").perform(context), 
             'roll': LaunchConfiguration("pose_roll").perform(context), 
             'yaw': LaunchConfiguration("pose_yaw").perform(context), 
-            'pitch': LaunchConfiguration("pose_pitch").perform(context), 
-            'publish_pointcloud': LaunchConfiguration("publish_pointcloud").perform(context), 
+            'pitch': LaunchConfiguration("pose_pitch").perform(context),
             'align_depth': LaunchConfiguration("align_depth").perform(context),
             'gazebo_urdf': LaunchConfiguration("gazebo_urdf").perform(context),
             'infra_enable': LaunchConfiguration("infra_enable").perform(context),
@@ -110,28 +109,42 @@ def launch_setup(context, *args, **kwargs):
     ))
 
     # Gz sim bridge to ROS2
+    bridge_topics = []
+    remappings = []
+
     if (LaunchConfiguration("align_depth").perform(context).lower() == "true"):
-        bridge_topics = [
+        bridge_topics.extend([
             # RGB Image topic is bridged with ros_gz_image
             f'/{LaunchConfiguration("name").perform(context)}/color/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
             f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
-            f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
-            f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/image_raw/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
-        ]
-        remappings = [
-            (f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/image_raw/points', 
-                f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/points')]
+        ])
+        
+        # Publish pointcloud only if enabled, as it is costly to compute
+        if (LaunchConfiguration("publish_pointcloud").perform(context).lower() == "true"):
+            bridge_topics.append(
+                f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/image_raw/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked'
+            )
+        
+            remappings.extend([
+                (f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/image_raw/points', 
+                    f'/{LaunchConfiguration("name").perform(context)}/aligned_depth_to_color/points')])
     else:
-        bridge_topics = [
+        bridge_topics.extend([
             # RGB Image topic is bridged with ros_gz_image
             f'/{LaunchConfiguration("name").perform(context)}/color/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
             f'/{LaunchConfiguration("name").perform(context)}/depth/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
             f'/{LaunchConfiguration("name").perform(context)}/depth/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
-            f'/{LaunchConfiguration("name").perform(context)}/depth/image_raw/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked',
-        ]
-        remappings = [
-            (f'/{LaunchConfiguration("name").perform(context)}/depth/image_raw/points', 
-                f'/{LaunchConfiguration("name").perform(context)}/depth/points')]
+        ])
+
+        # Publish pointcloud only if enabled, as it is costly to compute
+        if (LaunchConfiguration("publish_pointcloud").perform(context).lower() == "true"):
+            bridge_topics.append(
+                f'/{LaunchConfiguration("name").perform(context)}/depth/image_raw/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked'
+            )
+        
+            remappings.extend([
+                (f'/{LaunchConfiguration("name").perform(context)}/depth/image_raw/points', 
+                    f'/{LaunchConfiguration("name").perform(context)}/depth/points')])
         
     # Infrared bridging
     if (LaunchConfiguration("infra_enable").perform(context).lower() == "true"):
@@ -141,6 +154,11 @@ def launch_setup(context, *args, **kwargs):
             f'/{LaunchConfiguration("name").perform(context)}/infra2/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
             f'/{LaunchConfiguration("name").perform(context)}/infra2/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
         ])
+
+    # IMU bridging
+    bridge_topics.append(
+        f'/{LaunchConfiguration("name").perform(context)}/imu@sensor_msgs/msg/Imu[gz.msgs.IMU'
+    )
 
     # RGB image uses ros_gz_image bridge and can be remapped per camera name.
     nodes_to_start.append(
@@ -191,6 +209,12 @@ def generate_launch_description():
     # declared_arguments.append(DeclareLaunchArgument("spawn_roll", default_value="0.0"))
     # declared_arguments.append(DeclareLaunchArgument("spawn_pitch", default_value="0.0"))
     # declared_arguments.append(DeclareLaunchArgument("spawn_yaw", default_value="0.0"))
+
+
+    ############
+    # NOTE: Pointcloud are published always by Depth Camera plugin, but the 'publish_pointcloud'
+    #       argument controls whether the pointcloud topic is bridged to ROS2 or not
+    ############
     declared_arguments.append(DeclareLaunchArgument("publish_pointcloud", default_value="false"))
     declared_arguments.append(DeclareLaunchArgument("align_depth", default_value="false"))
     declared_arguments.append(DeclareLaunchArgument("gazebo_urdf", default_value="true"))
